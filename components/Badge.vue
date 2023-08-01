0c232bababa1df5ac8d76d4fc5065f49391bb0f0
navis23 <napisszarkasy@gmail.com>
2023-08-01 11:04:59 +0700

    fix(components): correction color to badge and icon btn. Fix-002

--- components/Badge.vue
+++ components/Badge.vue
@@ -1,5 +1,5 @@
 <template>
-    <span class="bg-sky-300 text-sky-700 bg-opacity-30 h-6 px-2 font-semibold text-xs flex justify-center items-center rounded-md">
+    <span class="bg-sky-200 text-sky-700 bg-opacity-30 h-6 px-2 font-semibold text-xs flex justify-center items-center rounded-md">
         <slot />
     </span>
 </template>
@@ -10,16 +10,16 @@
 
 <style lang="postcss" scoped>
 .danger {
-    @apply bg-red-300 text-red-700 
+    @apply bg-red-200 text-red-700 
 }
 .warning {
-    @apply bg-yellow-300 text-yellow-700 
+    @apply bg-yellow-200 text-yellow-700 
 }
 .muted {
-    @apply bg-slate-300 text-slate-700
+    @apply bg-slate-200 text-slate-700
 }
 .mono {
-    @apply bg-slate-50 text-gray-800 border
+    @apply bg-slate-50 text-gray-500 border
 }
 .dark {
     @apply text-slate-50 bg-gray-800
--- components/ButtonIcon.vue
+++ components/ButtonIcon.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="bg-sky-300 text-sky-700 bg-opacity-30 w-12 h-12 text-3xl flex justify-center items-center rounded-full hover:bg-sky-500 hover:text-gray-100 transition ease-in-out duration-300">
+    <button class="bg-sky-200 text-sky-700 bg-opacity-30 w-12 h-12 text-3xl flex justify-center items-center rounded-full hover:bg-sky-500 hover:text-gray-100 transition ease-in-out duration-300">
         <slot />
     </button>
 </template>
@@ -10,13 +10,13 @@
 
 <style lang="postcss" scoped>
 .danger {
-    @apply bg-red-300 text-red-700 hover:bg-red-500 hover:text-gray-100
+    @apply bg-red-200 text-red-700 hover:bg-red-500 hover:text-gray-100
 }
 .warning {
-    @apply bg-yellow-300 text-yellow-700 hover:bg-yellow-500 hover:text-gray-100
+    @apply bg-yellow-200 text-yellow-700 hover:bg-yellow-500 hover:text-gray-100
 }
 .muted {
-    @apply bg-slate-300 text-slate-700 hover:bg-slate-400 hover:text-gray-100
+    @apply bg-slate-200 text-slate-700 hover:bg-slate-400 hover:text-gray-100
 }
 .square {
     @apply rounded-md
